202222190acc427899bcdf3a1a4da965b35db174
Pim <PimTH@users.noreply.github.com>
2016-05-14 19:20:15 +0200

    Update Objecten.py
    Beweegfunctie verbeterd.

--- Objecten.py
+++ Objecten.py
@@ -1,4 +1,6 @@
 class Robot:
+    snelheidsverandering_x = 1
+    snelheidsverandering_y = 1
     def __init__(zelf, x=0, y=0, snelheid_x=5, snelheid_y=5, grootte_x=50, grootte_y=50):
        zelf.x = x
        zelf.y = y
@@ -9,11 +11,14 @@ class Robot:
     # simpele beweging
     def beweeg(zelf, richting=""):
         if "R" in list(richting):
-            zelf.x += zelf.snelheid_x
+            zelf.snelheid_x += snelheidsverandering_x
         if "L" in list(richting):
-            zelf.x -= zelf.snelheid_x
+            zelf.snelheid_x -= snelheidsverandering_x
         if "O" in list(richting):
-            zelf.y += zelf.snelheid_y
+            zelf.snelheid_y += snelheidsverandering_y
         if "N" in list(richting):
-            zelf.y += zelf.snelheid_y
+            zelf.snelheid_y -= snelheidsverandering_y
+        zelf.x += zelf.snelheid_x
+        zelf.y += zelf.snelheid_y
+
 
