7a0b42de3f243630ff05f0e8a0f02206c08138ce
Pim <pim-is@live.nl>
2016-05-15 20:54:45 +0200

    Beweeggedeelte
    Beweeggedeelte toegevoegd en kleine verbeteringen gedaan.

--- Objecten.py
+++ Objecten.py
@@ -1,10 +1,10 @@
-import tkinter
+import tkinter, random
 
 class Robot:
     id = 0
     kracht = 2500
     kleuren = ["white", "black", "red", "green", "blue", "cyan", "yellow", "magenta"]
-    def __init__(zelf, x=0, y=0, kleur="black", snelheid_x=5, snelheid_y=5, grootte_x=50, grootte_y=50):
+    def __init__(zelf, x=0, y=0, kleur="black", grootte_x=50, grootte_y=50, snelheid_x=0, snelheid_y=0):
         zelf.x = x
         zelf.y = y
         zelf.snelheid_x = snelheid_x
@@ -18,17 +18,28 @@ class Robot:
         Robot.id += 1
         zelf.id = Robot.id
     # simpele beweging
-    def beweeg(zelf, richting=0):
+    def beweeg(zelf, raam):
+        global speler
+        richting = 0
+        if zelf.id != speler.id:
+            richting = random.randint(0, 15)
+        else:
+            if raam.pressed["w"]: richting += 4
+            if raam.pressed["a"]: richting += 2
+            if raam.pressed["s"]: richting += 8
+            if raam.pressed["d"]: richting += 1
         if richting & 1 == 1:
-            zelf.snelheid_x += snelheidsverandering_x
+            zelf.snelheid_x += zelf.snelheidsverandering
         if richting & 2 == 2:
-            zelf.snelheid_x -= snelheidsverandering_x
+            zelf.snelheid_x -= zelf.snelheidsverandering
         if richting & 4 == 4:
-            zelf.snelheid_y += snelheidsverandering_y
+            zelf.snelheid_y += zelf.snelheidsverandering
         if richting & 8 == 8:
-            zelf.snelheid_y -= snelheidsverandering_y
+            zelf.snelheid_y -= zelf.snelheidsverandering
         zelf.x += zelf.snelheid_x
         zelf.y += zelf.snelheid_y
+    def teken(zelf, doek, ijkpunt):
+        doek.create_rectangle(zelf.x + ((x / 2) - ijkpunt[0]), -zelf.y + ((y / 2) + ijkpunt[1]), zelf.x + ((x / 2) - ijkpunt[0]) + zelf.grootte_x, -zelf.y + ((y / 2) + ijkpunt[1]) + zelf.grootte_y, fill = zelf.kleur)
 
 class Speler():
     def __init__(zelf, robotid):
@@ -50,23 +61,40 @@ class Doek(tkinter.Canvas):
         zelf.config(width=zelf.breedte, height=zelf.hoogte)
 
 class Raam:
-    global robots
     def __init__(zelf):
+        zelf.pressed = {}
         zelf.scherm = tkinter.Tk()
         zelf.scherm.title("Relativiteitstheorie")
         zelf.doek = Doek(zelf.scherm, width=x, height=y, bg="white", highlightthickness=0, border=0)
         zelf.doek.pack(fill=tkinter.BOTH, expand=tkinter.YES)
+        zelf._set_bindings()
         zelf.teken(zelf.doek)
+        zelf.doek.mainloop()
         zelf.scherm.mainloop()
 
+    def _set_bindings(zelf):
+        for char in ["w", 'a', 's', 'd']:
+            zelf.scherm.bind("<KeyPress-%s>" % char, zelf._pressed)
+            zelf.scherm.bind("<KeyRelease-%s>" % char, zelf._released)
+            zelf.pressed[char] = False
+
+    def _pressed(zelf, event):
+        zelf.pressed[event.char] = True
+
+    def _released(zelf, event):
+        zelf.pressed[event.char] = False
+
     def teken(zelf, doek):
+        zelf.doek.delete("all")
         ijkpunt = [speler.x + (speler.grootte_x / 2), speler.y - (speler.grootte_y / 2)]
         for robot in robots:
-            doek.create_rectangle(robot.x + ((x / 2) - ijkpunt[0]), -robot.y + ((y / 2) + ijkpunt[1]), robot.x + ((x / 2) - ijkpunt[0]) + robot.grootte_x, -robot.y + ((y / 2) + ijkpunt[1]) + robot.grootte_y, fill = robot.kleur)
+            robot.beweeg(zelf)
+            robot.teken(doek, ijkpunt)
+        zelf.doek.after(20, zelf.teken, doek)
 
 x = 800
 y = 800
-robots = [Robot(200, 100, "cyan"), Robot(600, 0, "red"), Robot(190, 0)]
+robots = [Robot(200, 100, "cyan", 200, 200), Robot(600, 0, "red"), Robot(190, 0)]
 speler = Speler(2)
 
 def main():
