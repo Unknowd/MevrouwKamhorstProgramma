f018b0c415a93e8c7df4d32b6fd2d31cd4b12292
Pim <PimTH@users.noreply.github.com>
2016-05-25 19:21:20 +0200

    Informatiescherm, robot-maken en kleuren
    Informatiescherm toegevoegd, robot-maken geautomatiseerd en kleurverbeteringen gedaan.

--- Objecten.py
+++ Objecten.py
@@ -1,4 +1,4 @@
-import tkinter, random
+import tkinter, random, math
 
 class Robot:
     # statische variable
@@ -6,7 +6,7 @@ class Robot:
     kracht = 2500
     kleuren = ["black", "red", "green", "blue", "cyan", "yellow", "magenta"]
 
-    def __init__(zelf, x=0, y=0, kleur="black", bkleur="black", ckleur="black", snelheid_x=5, snelheid_y=5, grootte_x=50, grootte_y=50):
+    def __init__(zelf, x=0, y=0, kleur="black", bkleur="black", snelheid_x=random.randint(-10, 10), snelheid_y=random.randint(-10, 10), grootte_x=50, grootte_y=50):
         zelf.x = x
         zelf.y = y
         zelf.snelheid_x = snelheid_x
@@ -16,13 +16,11 @@ class Robot:
         zelf.snelheidsverandering = Robot.kracht / (zelf.grootte_x * zelf.grootte_y)
         zelf.kleur = kleur
         if zelf.kleur not in Robot.kleuren:
-            zelf.kleur = "black"
+            zelf.kleur = Robot.kleuren[random.randint(0, len(Robot.kleuren) - 1)]
+        del Robot.kleuren[Robot.kleuren.index(zelf.kleur)]
         zelf.bkleur = bkleur
         if zelf.bkleur not in Robot.kleuren:
-            zelf.bkleur = "black"
-        zelf.ckleur = ckleur
-        if zelf.ckleur not in Robot.kleuren:
-            zelf.ckleur = "black"
+            zelf.bkleur = zelf.kleur
         Robot.id += 1
         zelf.id = Robot.id
  
@@ -32,7 +30,6 @@ class Robot:
         
     def teken(zelf, doek, x, y):
         doek.create_rectangle(zelf.x - x, zelf.y - y , zelf.x + zelf.grootte_x - x, zelf.y + zelf.grootte_y - y, fill = zelf.kleur, outline = zelf.bkleur, width = zelf.grootte_x//10)
-        doek.create_oval(zelf.x - x, zelf.y - y, zelf.x + zelf.grootte_x - x, zelf.y + zelf.grootte_y - y, fill = zelf.kleur, outline = zelf.ckleur, width = zelf.grootte_x//10)
         
 class Speler(Robot):
     def __init__(zelf, robot):
@@ -60,12 +57,17 @@ class Doek(tkinter.Canvas):
         zelf.hoogte = event.height
         zelf.config(width=zelf.breedte, height=zelf.hoogte)
 
-    def ververs(zelf, objecten, x, y):
+    def ververs(zelf, robots, x, y, speler, informatieweergeven):
         zelf.delete('all')
         x -= zelf.breedte//2
         y -= zelf.hoogte//2
-        for i in objecten:
+        for i in robots:
             i.teken(zelf, x, y)
+        if informatieweergeven:
+            snelheidstekst = ""
+            for robot in robots:
+                snelheidstekst += "\n" + robot.kleur + ": " + str(int(math.sqrt((speler.snelheid_x - robot.snelheid_x)**2 + (speler.snelheid_y - robot.snelheid_y)**2)))
+            zelf.create_text(5, 5, text = "Snelheden" + snelheidstekst, fill = "black", font = "-size 30", anchor="nw")
 
     def far_away(zelf, robot, x, y):
         robot.x -= x
@@ -80,6 +82,9 @@ class Doek(tkinter.Canvas):
             
 class Raam:
     def __init__(zelf):
+        zelf.tijd = 0
+        zelf.verbodentijd = 0
+        zelf.informatieweergeven = False
         zelf.pressed = {}
         zelf.scherm = tkinter.Tk()
         zelf.scherm.title("Relativiteitstheorie")
@@ -88,16 +93,16 @@ class Raam:
         zelf.scherm.geometry("{0}x{1}+{2}+{3}".format(x, y, x - x//2, y - y//2))
         zelf.doek = Doek(zelf.scherm, bg="white", highlightthickness=0, border=0)
         zelf.doek.pack(fill=tkinter.BOTH, expand=tkinter.YES)
-        zelf.robots = [Robot(10, 10, kleur = "cyan", bkleur = "green", ckleur = "blue"), Robot(50,50)]
+        zelf.robots = [Robot(random.randint(0,x), random.randint(0,y), snelheid_x=random.randint(-10, 10), snelheid_y=random.randint(-10, 10)), Robot(random.randint(0,x), random.randint(0,y), snelheid_x=random.randint(-10, 10), snelheid_y=random.randint(-10, 10)), Robot(random.randint(0,x), random.randint(0,y), snelheid_x=random.randint(-10, 10), snelheid_y=random.randint(-10, 10))]
         zelf.speler = Speler(zelf.robots[0])
         zelf.state = False
         zelf._set_bindings()
         zelf.uitvoeren()
 
     def _set_bindings(zelf):
-        zelf.scherm.bind("<Button-1>", zelf._click)
+        zelf.scherm.bind("<Button-1>", zelf._leftclick)
         zelf.scherm.bind("<F11>", zelf._toggle_volledigscherm)
-        for char in ["w", 'a', 's', 'd']:
+        for char in ["w", "a", "s", "d", "r", "i"]:
             zelf.scherm.bind("<KeyPress-%s>" % char, zelf._pressed)
             zelf.scherm.bind("<KeyRelease-%s>" % char, zelf._released)
             zelf.pressed[char] = False
@@ -106,7 +111,7 @@ class Raam:
         zelf.state = not zelf.state
         zelf.scherm.attributes("-fullscreen", zelf.state)
         
-    def _click(zelf, event):
+    def _leftclick(zelf, event):
         robot = zelf.doek.vind_robot(zelf.robots, event.x, event.y)
         if robot:
             zelf.speler = Speler(robot)
@@ -120,15 +125,29 @@ class Raam:
     def hergroepeer(zelf):
         x, y = zelf.speler.x - zelf.doek.breedte//2 + zelf.speler.grootte_x//2, zelf.speler.y - zelf.doek.hoogte//2 - zelf.speler.grootte_y//2
         zelf.robots = [robot for robot in zelf.robots if not zelf.doek.far_away(robot, x, y)]
+
+    def knopcontroleren(zelf):
+        if (zelf.pressed["r"] or zelf.pressed["i"]) and zelf.tijd - 75 > zelf.verbodentijd:
+            if zelf.pressed["r"] and len(zelf.robots) < 7:
+                zelf.robots.append(Robot(50, 50, snelheid_x=random.randint(-10, 10), snelheid_y=random.randint(-10, 10)))
+            if zelf.pressed["i"]:
+                if zelf.informatieweergeven:
+                    zelf.informatieweergeven = False
+                else:
+                    zelf.informatieweergeven = True
+            zelf.tijd = 0
+            zelf.verbodentijd = zelf.tijd
         
     def uitvoeren(zelf):
+        zelf.tijd += 20
         s = zelf.speler
         x, y = s.x + (s.grootte_x // 2), s.y + (s.grootte_y // 2)
         s.ververs_snelheid(zelf.pressed)
+        zelf.knopcontroleren()
         for i in zelf.robots:
             i.beweeg()
         zelf.hergroepeer()
-        zelf.doek.ververs(zelf.robots, x, y)
+        zelf.doek.ververs(zelf.robots, x, y, s, zelf.informatieweergeven)
         zelf.scherm.after(20, zelf.uitvoeren)
 
 def main():
